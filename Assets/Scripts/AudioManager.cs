12380dd9edf2fc1443624f3bc24aa9035453259e
ArtsolutionS <ischenko.andrehantey@gmail.com>
2025-02-21 18:43:04 +0100

    Interact, Press button and computer sounds

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -16,17 +16,25 @@ public class AudioManager : MonoBehaviour
     [SerializeField] AudioSource jumpSound;
     [SerializeField] AudioSource interactSound;
     [SerializeField] AudioSource pressedButtonSound;
-    [SerializeField] AudioSource computerSound;
+    [SerializeField] List<AudioSource> computerSounds = new List<AudioSource>();
 
     private void Awake()
     {
         if (instance == null) instance = this;
+
+        //A List of public methods:
         //PlayIntroSound();
         //PlayStepSound();
         //StopStepSound();
+
         //PlayJumpSound();
+        //PlayInteractSound();
+        //PlayPressButtonSound();
         //PlayBlaBlaMale();
         //PlayBlaBlaFemale();
+
+        //PlayComputerStartUpSound();
+        //PlayComputerShutDownSound();
     }
 
 
@@ -68,7 +76,30 @@ public class AudioManager : MonoBehaviour
     }
     public void PlayInteractSound()
     {
-
+        interactSound.Play();
+    }
+    public void PlayPressButtonSound()
+    {
+        pressedButtonSound.Play();
+    }
+    public async void PlayComputerStartUpSound()
+    {
+        computerSounds[0].volume = 1f;
+        computerSounds[0].Play();
+        await Task.Delay(3000);
+        computerSounds[1].Play();
+        await Task.Delay(1200);
+        computerSounds[0].volume = 0.6f;
+        await Task.Delay(500);
+        computerSounds[0].volume = 0.3f;
+        await Task.Delay(200);
+        computerSounds[0].volume = 0.1f;
+        await Task.Delay(200);
+        computerSounds[0].volume = 0f;
+    }
+    public void PlayComputerShutDownSound()
+    {
+        computerSounds[2].Play();
     }
 
     #region step sounds

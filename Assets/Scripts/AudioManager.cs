868346f860286965e019cb8f5a279e898e6b1ab0
ArtsolutionS <ischenko.andrehantey@gmail.com>
2025-02-21 17:00:30 +0100

    Random steps random pitch + blabla sounds added

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -6,12 +6,25 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
     [SerializeField] float stepSoundRate;
+    int randomIndex = 0;
+    [Header("Sounds")]
     [SerializeField] AudioSource introSound;
-    [SerializeField] AudioSource stepSound;
+    [SerializeField] List<AudioSource> stepSounds = new List<AudioSource>();
+
     private void Awake()
     {
         if (instance == null) instance = this;
+        PlayStepSound();
     }
+
+
+    #region helper functions
+    float GetRandomPitch()
+    {
+        return Random.Range(1f, 1.8f);
+    }
+    #endregion
+
 
 
     public void PlayIntroSound()
@@ -28,6 +41,9 @@ public class AudioManager : MonoBehaviour
     }
     void StepSoundRepetition()
     {
-        stepSound.Play();
+        Debug.Log("Play sound step " + randomIndex);
+        randomIndex = Random.Range(0, stepSounds.Count);
+        stepSounds[randomIndex].pitch = GetRandomPitch();
+        stepSounds[randomIndex].Play();
     }
 }

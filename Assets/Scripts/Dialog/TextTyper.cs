77250ea3afa47a5bfd1a03ecc308c8d10a6930bf
Dmitrii Puntusov <puntusovdima@gmail.com>
2025-02-22 10:08:36 +0100

    Little change in text typer

--- Assets/Scripts/Dialog/TextTyper.cs
+++ Assets/Scripts/Dialog/TextTyper.cs
@@ -26,6 +26,8 @@ public class TextTyper : MonoBehaviour
         // string output;
         for (int i = 0; i < text.Length; i++)
         {
+            if (Input.anyKeyDown)
+                typeAll = true;
             if (typeAll)
             {
                 textOut.text = text;
